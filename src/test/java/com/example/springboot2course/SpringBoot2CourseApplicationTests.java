77233fc24605ed64a2b5404cac01c9fbd6181fa5
Grzegorz Kowalczyk <86222743+gkowalczyk@users.noreply.github.com>
2025-01-01 14:04:18 +0100

    initial commit

---
+++ src/test/java/com/example/springboot2course/SpringBoot2CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot2course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot2CourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
